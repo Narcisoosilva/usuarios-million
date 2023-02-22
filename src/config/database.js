92f84c368c7f19847d4126cdc8abeedbe8c47b68
Narcisoosilva <narcisoosilva@gmail.com>
2023-02-22 20:32:39 -0300

    MONGO_URI
    Colocando  variável de ambiente do banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,7 @@ require('dotenv').config({
     path: '.env'
 })
 const mongoose = require('mongoose');
-
-const uri = process.env.MONGO_URI;
+const uri = process.env.MONGO_URI ? process.env.MONGO_URI : 'mongodb://localhost:27017/usuarios-million';
 
 mongoose.connect(uri,  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
 mongoose.Promise = global.Promise;
